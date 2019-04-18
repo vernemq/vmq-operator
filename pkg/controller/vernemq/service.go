88055a728ad89c4b64a7da019df1e6705b84e8f2
Andre Graf <andre@dergraf.org>
2019-04-18 10:09:07 +0200

    Refactoring

---
+++ pkg/controller/vernemq/service.go
@@ -0,0 +1,54 @@
+package vernemq
+
+import (
+	"fmt"
+
+	vernemqv1alpha1 "github.com/vernemq/vmq-operator/pkg/apis/vernemq/v1alpha1"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+)
+
+func makeStatefulSetService(instance *vernemqv1alpha1.VerneMQ) *v1.Service {
+
+	svc := &v1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			OwnerReferences: []metav1.OwnerReference{
+				{
+					APIVersion: instance.APIVersion,
+					Kind:       instance.Kind,
+					Name:       instance.Name,
+					UID:        instance.UID,
+				},
+			},
+			Labels: map[string]string{
+				"operated-vernemq": "true",
+			},
+		},
+		Spec: v1.ServiceSpec{
+			ClusterIP: "None",
+			Ports: []v1.ServicePort{
+				{
+					Name:       "mqtt",
+					Port:       1883,
+					TargetPort: intstr.FromString("mqtt"),
+				},
+				{
+					Name:       "http",
+					Port:       8888,
+					TargetPort: intstr.FromString("http"),
+				},
+			},
+			Selector: map[string]string{
+				"app": "vernemq",
+			},
+		},
+	}
+	svc.Name = serviceName(instance.Name)
+	svc.Namespace = instance.Namespace
+	return svc
+}
+
+func serviceName(name string) string {
+	return fmt.Sprintf("%s-service", prefixedName(name))
+}
