1dbeae195d4cdf9d7dc8fdb05e642ce8c8f36b9a
Lars Hesel Christensen <lars@larshesel.dk>
2019-06-20 10:14:43 +0200

    Configure plugins via commands

--- erlang/src/vmq_k8s_reloader.erl
+++ erlang/src/vmq_k8s_reloader.erl
@@ -151,17 +151,21 @@ apply_plugins_config([PluginConfig|Rest], Acc, CurrentState) ->
             case maps:is_key({plugin, Name}, CurrentState) of
                 true ->
                     % already installed
-                    apply_plugins_config(Rest, maps:put({plugin, Name}, enabled, Acc), CurrentState);
+                    apply_plugins_config(Rest, maps:put({plugin, Name}, PluginConfig, Acc), CurrentState);
                 false ->
+                    PreCmds = proplists:get_value("preStart", PluginConfig, []),
+                    ok = exec_commands(PreCmds),
                     Acc1 =
                     case proplists:get_value("path", PluginConfig) of
                         undefined ->
                             command(["plugin", "enable", "-n", Name],
-                                    succf({plugin, Name}, enabled), Acc);
+                                    succf({plugin, Name}, PluginConfig), Acc);
                         Path ->
                             command(["plugin", "enable", "-n", Name, "-p", Path],
-                                    succf({plugin, Name}, enabled), Acc)
+                                    succf({plugin, Name}, PluginConfig), Acc)
                     end,
+                    PostCmds = proplists:get_value("preStart", PluginConfig, []),
+                    ok = exec_commands(PostCmds),
                     apply_plugins_config(Rest, Acc1, CurrentState)
             end
     end;
@@ -170,12 +174,29 @@ apply_plugins_config([], NewState, OldState) ->
     Old = [Name || {plugin, Name} <- maps:keys(OldState)],
     ToBeDisabled = Old -- New,
     lists:foreach(fun(Name) ->
-                          command(["plugin", "disable", "-n", Name])
+                          Cfg = maps:get(OldState, {plugin, Name}),
+                          PreCmds = proplists:get_value("preStop", Cfg, []),
+                          exec_commands(PreCmds),
+                          command(["plugin", "disable", "-n", Name]),
+                          PostCmds = proplists:get_value("preStop", Cfg, []),
+                          exec_commands(PostCmds)
                   end, ToBeDisabled),
     NewState;
 apply_plugins_config(null, NewState, _OldState) ->
     %% empty list is decoded as null
     NewState.
+
+exec_commands([]) -> ok;
+exec_commands([CmdConfig|Rest]) ->
+    _TimeoutSeconds = proplists:get_value("timeoutSeconds", CmdConfig, 5),
+    Cmd = proplists:get_value("cmd", CmdConfig),
+    Redirect =
+        case proplists:get_value("redirectStderr", CmdConfig, true) of
+            true -> " 2>&1";
+            false -> ""
+        end,
+    os:cmd([Cmd, Redirect]),
+    exec_commands(Rest).
 
 to_snake_case(S) ->
     string:lowercase(re:replace(S, "[A-Z]", "_&", [{return, list}, global])).
